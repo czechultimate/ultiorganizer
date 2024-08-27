2c672fc0b141725b20fcb11f957e1d87954fe26b
W4jzy <tomas.weisheitel@gmail.com>
2024-08-27 09:29:40 +0200

    Minor bug fix

--- lib/pool.functions.php
+++ lib/pool.functions.php
@@ -272,7 +272,7 @@ function PoolPlacementString($poolId, $pos, $ordinal = true)
     $placementfrom = 1;
     foreach ($ppools as $ppool) {
       
-      $teams = PoolTeams($ppool['pool_id']);
+      $teams = PoolSchedulingTeams($ppool['pool_id']);
       if ($info['pool_id'] != $ppool['pool_id']) {
         for ($i = 1; $i <= count($teams); $i++) {
           $moved = PoolMoveExist($ppool['pool_id'], $i);
